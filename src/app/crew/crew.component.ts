f5647cc80d84765ba23a874133355793f3fab9ce
Kurtphilly <kurt@none>
2022-07-03 09:44:17 -0400

    Compl Ang Studio 2

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-crew',
@@ -9,10 +10,28 @@ export class CrewComponent implements OnInit {
 
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
-    {name: "Mae Jemison", firstMission: false},
-    {name: "Ellen Ochoa", firstMission: true}
+    {name: "Mae Jemison ", firstMission: false},
+    {name: "Ellen Ochoa ", firstMission: true}
   ];
+add(memberName: string, isFirst: boolean) {
+  this.crew.push({name: memberName, firstMission: isFirst});
 
+
+}
+remove(member: object){
+  let index= this.crew.indexOf(member);
+  this.crew.splice(index, 1);
+}
+memberBeingEdited: object = null;
+
+edit(member: object){
+  this.memberBeingEdited = member;
+}
+
+save(name: string, member: object){
+  member['name'] = name;
+  this.memberBeingEdited = null;
+}
   constructor() { }
 
   ngOnInit() {
